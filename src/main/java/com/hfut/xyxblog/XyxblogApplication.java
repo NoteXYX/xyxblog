83d594b0cac26c878d0d9fcaef86c4bf7756d881
xieyuxin <707997228@qq.com>
2020-11-25 21:48:08 +0800

    first commit

---
+++ src/main/java/com/hfut/xyxblog/XyxblogApplication.java
@@ -0,0 +1,13 @@
+package com.hfut.xyxblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyxblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyxblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hfut/xyxblog/XyxblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hfut.xyxblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyxblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
